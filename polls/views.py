5a85f02102b7d6927ee6f74147e349eb2a9f340b
donghee <dlehdgml5328@naver.com>
2025-06-04 14:07:25 +0900

    [feat]

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,48 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question, Choice
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.db.models import F
 
-# index(최신글 list)
-def index(request):
-	# return HttpResponse("Hello) 기존코드
-	
-	latest_question_list = Question.objects.order_by("-pub_date")[:5]
-	context = {"latest_question_list": latest_question_list}
-	return render(request, "polls/index.html", context)
+from .models import Question, Choice  # ← 이거 중요!
 
-def detail(request,question_id):
-    question =get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/detail.html", {"question": question})
+# 메인 페이지 (질문 목록)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
+
+# 질문 상세 페이지
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    context_object_name = "question"
+
+
+# 결과 페이지
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    context_object_name = "question"
+
+# 투표 처리 로직
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "선택을하고 눌러라!!!!!!!!!!",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
